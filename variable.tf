faac9a4f587465105ee161913998f326b8d6633c
8743832296 <48963753+8743832296@users.noreply.github.com>
2022-06-24 13:00:42 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "accessKey" {}
+variable "secretKey" {}
+variable "instance_name" {
+type = string
+default = "testvm"
+}
+
+variable "sn_instance" {
+type = string
+default = "DoNotModify"
+}
